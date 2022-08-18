93bc260308c581d0ac052d301802b5e6cff55b4d
Nicholas <nicholaseiti@gmail.com>
2022-08-18 12:28:48 -0300

    commit

---
+++ php_page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>teste de git</title>
+</head>
+<body>
+    <p>Olá, eu sou uma página em PHP</p>
+    <?php
+        $nome = "PHP_PAGE";
+        echo "Meu nome é " . $nome;
+    ?>
+</body>
+</html>
